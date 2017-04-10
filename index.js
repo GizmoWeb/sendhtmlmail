c8a21c4ada14695d24eac9b92e38c98a0d75061b
Daniele Tardia <daniele.tardia@fulltechnology.it>
2017-04-10 11:06:40 +0200

    Upgrade js sintax added minimist to manage command line params added conf param for alternative configuration file

--- index.js
+++ index.js
@@ -1,218 +1,286 @@
-/*jslint devel: true, node: true, nomen: true, regexp: true*/
 'use strict';
-var path = require('path'),
-	deepExtend = require('deep-extend'),
-	nodemailer = require('nodemailer'),
-	fs = require('fs'),
-	prompt = require('prompt'),
-	colors = require("colors/safe"),
-	uuid = require('node-uuid'),
-	pkg = require(__dirname + '/package.json'),
-	_appRoot = process.cwd(), //path.resolve(__dirname)
-	_confFile,
-	_configuration,
-	_pageHTML,
-	_pageHTMLFolder,
-	_cmdParams,
-	_attachments = [],
-	_promptSchema = {
-		properties: {
-			use_images: {
-				name: 'use_images',
-				pattern: /[Y|n|y|N]/,
-				description: 'Send with images? [Y|n]',
-				message: 'Answer "Y" or "n"',
-				required: true
-			},
-			embed_images: {
-				name: 'embed_images',
-				pattern: /[Y|n|y|N]/,
-				description: 'Use attached images? [Y|n]',
-				message: 'Answer "Y" or "n"',
-				required: true
-			}
-		}
-	};
+// const path = require('path');
+const deepExtend = require('deep-extend');
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const prompt = require('prompt');
+const colors = require('colors/safe');
+const uuid = require('node-uuid');
+const pkg = require(__dirname + '/package.json');
+let _appRoot = process.cwd(); // path.resolve(__dirname)
+let _confFile;
+let _configuration;
+let _pageHTML;
+let _pageHTMLFolder;
+let _cmdParams;
+let _callback;
+let _attachments = [];
+let _promptSchema = {
+        properties: {
+            useImages: {
+                name: 'useImages',
+                pattern: /[Y|n|y|N]/,
+                description: 'Send with images? [Y|n]',
+                message: 'Answer "Y" or "n"',
+                required: true,
+            },
+            embedImages: {
+                name: 'embedImages',
+                pattern: /[Y|n|y|N]/,
+                description: 'Use attached images? [Y|n]',
+                message: 'Answer "Y" or "n"',
+                required: true,
+            },
+        },
+    };
 /**
  * Trace console messages
  * @private
  * @param {*}   el
  * @param {string}   message
- * @param {string}	 type    
+ * @param {string}     type
  */
-function _trace(el, message, type) {
-	if (type === "err") {
-		console.error("##### " + el + " ####\n", message, "\n######################################");
-	} else {
-		console.log(el);
-	}
-}
-function _uncaughtExceptionHandler(err) {
-	_trace("ERRORE INATTESO", err.message, "err");
-}
+let _trace = (el, message, type) => {
+    if (type === 'err') {
+        console.error(
+            '##### ' + el + ' ####\n',
+            message,
+            '\n######################################');
+    } else {
+        console.warn(el);
+    }
+};
+/**
+ * Uncaught Exception Handler
+ * @param  {object} err
+ */
+let _uncaughtExceptionHandler = (err) => {
+    _trace('ERRORE INATTESO', err.message, 'err');
+};
 /**
  * Send mail use nodemailer
  * @private
- * @returns {void}
+ * @return {void}
  */
-function _sendMails() {
-	var mailOptions = {
-			from: _configuration.from, // sender address
-			//to: _configuration.to.join(','), // list of receivers
-			bcc: _configuration.to.join(','), // list of receivers
-			subject: _pageHTML.match(/<title>([A-z \-]*)<\/title>/i)[1], // Subject line
-			html: _pageHTML,
-			attachments: _attachments
-		},
-		transporter = nodemailer.createTransport(_configuration.smtpConfig); // create reusable transporter object using the default SMTP transport
-    
+let _sendMails = () => {
+    let mailOptions = {
+            from: _configuration.from, // sender address
+            // to: _configuration.to.join(','), // list of receivers
+            bcc: _configuration.to.join(','), // list of receivers
+            subject: _pageHTML.match(/<title>([A-z \-]*)<\/title>/i)[1], // Subject line
+            html: _pageHTML,
+            attachments: _attachments,
+        };
+    let transporter = nodemailer.createTransport(_configuration.smtpConfig); // create reusable transporter object using the default SMTP transport
+
     // send mail with defined transport object
-    transporter.sendMail(mailOptions, function (error, info) {
+    transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-			_trace('ERROR SENDING MAIL', error.message, "err");
+            _trace('ERROR SENDING MAIL', error.message, 'err');
             return;
         }
-        console.log('Message sent: ' + info.response);
+        console.warn('Message sent: ' + info.response);
+        _callback(info);
     });
-}
-function _replaceAndSetURL(match) {
+};
+/**
+ * Integrate configuration image base URL with matched RegExp part
+ * @param  {string} match
+ * @return {string}
+ */
+let _replaceAndSetURL = (match) => {
     return _configuration.imageBaseURL + match;
-}
+};
 /**
  * Replace img url with CID and add related element to attacchments array
  * @private
- * @param   {string} match 
- * @param   {string} p1    
- * @param   {string} p2    
- * @returns {string} calculated unique id for cid
+ * @param   {string} match
+ * @param   {string} p1
+ * @param   {string} p2
+ * @return  {string} calculated unique id for cid
  */
-function _replaceAndSetAttachments(match, p1, p2) {
-    var img_cid = null,
-		el,
-		fname,
-		rg = new RegExp('([^\\\\|^/].*[\\\\|/])(.+\\..+)$', 'gi');
-	for (el in _attachments) {
+let _replaceAndSetAttachments = (match, p1, p2) => {
+    let imgCID = null;
+    let el;
+    let fname;
+    let rg = new RegExp('([^\\\\|^/].*[\\\\|/])(.+\\..+)$', 'gi');
+    for (el in _attachments) {
         if (_attachments[el].path === _pageHTMLFolder + match) {
-            img_cid = _attachments[el].cid;
+            imgCID = _attachments[el].cid;
             break;
         }
     }
-    if (img_cid === null) {
-        img_cid = uuid.v4() + (_configuration.embedCIDDomain !== undefined && _configuration.embedCIDDomain !== null && _configuration.embedCIDDomain !== "" ? '@' + _configuration.embedCIDDomain : '');
+    if (imgCID === null) {
+        imgCID = uuid.v4() + (
+            _configuration.embedCIDDomain !== undefined
+            &&
+            _configuration.embedCIDDomain !== null
+            &&
+            _configuration.embedCIDDomain !== '' ? '@'
+            + _configuration.embedCIDDomain : ''
+        );
         fname = p2.match(rg) === null ? p2 : rg.exec(p2)[2];
         _attachments.push({
             filename: fname,
             path: _pageHTMLFolder + match,
-            cid: img_cid
+            cid: imgCID,
         });
     }
-	return 'cid:' + img_cid;
-}
+    return 'cid:' + imgCID;
+};
 /**
  * Start and manage user interaction
  * @private
  */
-function _prompt() {
-	prompt.message = colors.red("\n --- ");
-	prompt.delimiter = "";
-	prompt.start();
-	var use_images = 'Y',
-		embed_images = 'Y';
-	prompt.get(_promptSchema.properties.use_images, function (err, result) {
-		if (err) {
-			console.log("\n");
-			return;
-		}
-		use_images = result.use_images;
-		if (use_images.toUpperCase() === 'Y') {
-			prompt.get(_promptSchema.properties.embed_images, function (err, result) {
-				if (err) {
-					console.log("\n");
-					return;
-				}
-				embed_images = result.embed_images;
-				if (embed_images.toUpperCase() === 'Y') {
-					var path = new RegExp('(' + _configuration.imageFolder + ')(.+\\.[png|gif|jpg|svg]*)', 'igm');
-					_pageHTML = _pageHTML.replace(path, _replaceAndSetAttachments);
-				} else {
-					_pageHTML = _pageHTML.replace(/(\/assets\/img\/)([a-z\.A_Z0-9-]*)(?:\/|)([a-z\.A_Z0-9-]*)/gi , _replaceAndSetURL);
-				}
-				_sendMails();
-			});
-		} else {
-			_pageHTML = _pageHTML.replace(/<img([^>]*[^/])>/gi, '');
-			_pageHTML = _pageHTML.replace(/url\(.*\)/gi, '');
-			_sendMails();
-		}
-	});
-}
+let _prompt = () => {
+    prompt.message = colors.red('\n --- ');
+    prompt.delimiter = '';
+    prompt.start();
+
+    let useImages = 'Y';
+    let embedImages = 'Y';
+
+    prompt.get(_promptSchema.properties.useImages, (err, result) => {
+        if (err) {
+            console.error('\n');
+            return;
+        }
+        useImages = result.useImages;
+        if (useImages.toUpperCase() === 'Y') {
+            prompt.get(
+                _promptSchema.properties.embedImages,
+                (err, result) => {
+                    if (err) {
+                        console.error('\n');
+                        return;
+                    }
+                    embedImages = result.embedImages;
+                    if (embedImages.toUpperCase() === 'Y') {
+                        let path = new RegExp(
+                            '('
+                            + _configuration.imageFolder
+                            + ')(.+\\.[png|gif|jpg|svg]*)', 'igm'
+                        );
+                        _pageHTML = _pageHTML.replace(
+                            path,
+                            _replaceAndSetAttachments
+                        );
+                    } else {
+                        _pageHTML = _pageHTML.replace(/(\/assets\/img\/)([a-z\.A_Z0-9-]*)(?:\/|)([a-z\.A_Z0-9-]*)/gi, _replaceAndSetURL);
+                    }
+                    _sendMails();
+                }
+            );
+        } else {
+            _pageHTML = _pageHTML.replace(/<img([^>]*[^/])>/gi, '');
+            _pageHTML = _pageHTML.replace(/url\(.*\)/gi, '');
+            _sendMails();
+        }
+    });
+};
 /**
  * Handler for HTML email file read
  * @private
  * @param {Error} err
  * @param {Buffer} data
  */
-function _readPageHandler(err, data) {
-	if (err) {
-		_trace('ERROR LOADING HTML PAGE TO SEND', err.message, "err");
-	}
-	if (data) {
-		_pageHTMLFolder = _configuration.file.match(/[a-z:]*[\\|/]*[^\\|^/].*[\\|/]/i);
-		_pageHTML = data;
-		_prompt();
-	}
-}
+let _readPageHandler = (err, data) => {
+    if (err) {
+        _trace('ERROR LOADING HTML PAGE TO SEND', err.message, 'err');
+    }
+    if (data) {
+        _pageHTMLFolder = _configuration.file.match(/[a-z:]*[\\|/]*[^\\|^/].*[\\|/]/i);
+        _pageHTML = data;
+        _prompt();
+    }
+};
 /**
  * Get page to send
  * @private
- * @returns {Buffer} file descriptor
+ * @return {Buffer} file descriptor
  */
-function _getPage() {
-	_trace("*** loading : " + _configuration.file);
-	fs.readFile(_configuration.file, 'utf8', _readPageHandler);
-}
+let _getPage = () => {
+    _trace('*** loading : ' + _configuration.file);
+    return fs.readFile(_configuration.file, 'utf8', _readPageHandler);
+};
 /**
  * Handler for config file read
  * @private
  * @param {Error} err
  * @param {Buffer} data
  */
-function _readConfigHandler(err, data) {
-	if (err) {
-		_trace('ERROR LOADING CONFIGURATION', err.message, "err");
-	}
-	if (data) {
-		_confFile = data;
-		_configuration = JSON.parse(_confFile);
-		deepExtend(_configuration, _cmdParams);
-		if (_configuration.commandLine === undefined || _configuration.commandLine === null || _configuration.commandLine === false) {
-			prompt.override = {
-				use_images : _configuration.useImages !== undefined && _configuration.useImages !== null ? _configuration.useImages :  'Y',
-				embed_images : _configuration.embedImages !== undefined && _configuration.embedImages !== null ? _configuration.embedImages :  "Y"
-			};
-		}
-		if(_configuration.imageBaseURL !== undefined && _configuration.imageBaseURL !== null && _configuration.imageBaseURL !== ""){
-			_promptSchema.properties.embed_images.description = colors.italic.underline("Image base URL : " + _configuration.imageBaseURL) + colors.red("\n --- ") + _promptSchema.properties.embed_images.description;
-		}
-		_trace("\n");
-		_trace(_configuration)
-		_trace("\n");
-		_getPage();
-	}
-}
+let _readConfigHandler = (err, data) => {
+    if (err) {
+        _trace('ERROR LOADING CONFIGURATION', err.message, 'err');
+    }
+    if (data) {
+        _confFile = data;
+        _configuration = JSON.parse(_confFile);
+        deepExtend(_configuration, _cmdParams);
+        if (
+            _configuration.commandLine === undefined
+            ||
+            _configuration.commandLine === null ||
+            _configuration.commandLine === false
+        ) {
+            prompt.override = {
+                useImages:
+                    _configuration.useImages !== undefined
+                    && _configuration.useImages !== null
+                        ? _configuration.useImages
+                        : 'Y',
+                embedImages:
+                    _configuration.embedImages !== undefined
+                    && _configuration.embedImages !== null
+                        ? _configuration.embedImages
+                        : 'Y',
+            };
+        }
+        if(
+            _configuration.imageBaseURL !== undefined
+            &&
+            _configuration.imageBaseURL !== null
+            && _configuration.imageBaseURL !== ''
+        ) {
+            _promptSchema.properties.embedImages.description =
+                colors.italic.underline(
+                    'Image base URL : ' +
+                    _configuration.imageBaseURL
+                ) + colors.red('\n --- ')
+                + _promptSchema.properties.embedImages.description;
+        }
+        _trace('\n');
+        _trace(_configuration);
+        _trace('\n');
+        _getPage();
+    }
+};
 /**
  * Get _configuration from file
  * @private
  */
-function _getConfig() {
-	_trace("*** loading : " + _appRoot + '/conf/' + pkg.name + '_conf.json');
-	fs.readFile(_appRoot + '/conf/' + pkg.name + '_conf.json', 'utf8', _readConfigHandler);
-}
-
-function sendhtmlmail(params) {
-	process.on('uncaughtException', _uncaughtExceptionHandler);
-	_cmdParams = params;
-	_getConfig();
-}
+let _getConfig = () => {
+    let configFilePath = _cmdParams.conf !== undefined
+        ? _cmdParams.conf
+        : _appRoot + '/conf/' + pkg.name + '_conf.json';
+    _trace('*** loading : ' + configFilePath);
+    fs.readFile(
+        configFilePath,
+        'utf8',
+        _readConfigHandler
+    );
+};
+/**
+ * Send HTML mail using config file and specified HTML
+ * @param  {object} params
+ * @param  {object} callback
+ * @return {void}
+ */
+let sendhtmlmail = (params, callback) => {
+    process.on('uncaughtException', _uncaughtExceptionHandler);
+    _cmdParams = params;
+    _callback = callback;
+    _getConfig();
+};
 
 module.exports = sendhtmlmail;
